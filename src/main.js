7e25c38ff0329ea8163b054e80bceeeb5dd85dc4
Henry Chung <chungthehao@gmail.com>
2020-04-14 15:27:31 +0700

    Babel Polyfills, Transforms & Presets (use a specific polyfill, core-js/fn/promise)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ require("./index.html"); // Sau đó compile ra 1 file riêng (cấu hình ở w
 /**
  * Sau khi cài: "babel-polyfill" và cấu hình ở file webpack.dev.js (import nó trước source code của mình) thì:
  * IE11 cho dù không natively có tính năng 'promise' cũng đã chạy được code dùng 'promise'
+ * Mặc dù chạy ok vậy, nhưng việc import cả đống polyfill vô sẽ làm cho main-bundle.js nặng, giải pháp là import cái cần xài thôi
+ * main-bundle.js (giảm từ 849 KiB xuống 460KiB)
  */
 const myFunc = async () => {
   await console.log("Hi from the future!");
