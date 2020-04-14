0bc526a3cbe9ae7a56387d0fdba7d8c440082ba1
Henry Chung <chungthehao@gmail.com>
2020-04-14 15:08:26 +0700

    Babel Polyfills, Transforms & Presets (babel-plugin-async-to-promises)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 require("./main.css");
 require("./index.html"); // Sau đó compile ra 1 file riêng (cấu hình ở webpack.dev.js)
 
-const myFunc = () => console.log("Hi from the future!");
+/**
+ * Sau khi cài: "babel-plugin-async-to-promises" thì:
+ * Đã convert code từ async-await qua promise (1 số browser ko hiểu async-await, mà hiểu promise thì vẫn chạy ngon)
+ * Nhưng IE11 cũng ko hiểu promise nên sẽ có lỗi: 'Promise' is undefined (Sẽ dùng polyfill giải quyết)
+ */
+const myFunc = async () => {
+  await console.log("Hi from the future!");
+  console.log("Done!");
+};
 myFunc();
