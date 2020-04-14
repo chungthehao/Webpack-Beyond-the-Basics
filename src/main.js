309fad2f7c1a22564b891c5d00f8d2d8a9a5036e
Henry Chung <chungthehao@gmail.com>
2020-04-14 17:20:44 +0700

    Babel Polyfills, Transforms & Presets (presets)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
+require("core-js");
+require("regenerator-runtime/runtime");
+
 require("./main.css");
 require("./index.html"); // Sau đó compile ra 1 file riêng (cấu hình ở webpack.dev.js)
 
 /**
- * Sau khi cài: "babel-polyfill" và cấu hình ở file webpack.dev.js (import nó trước source code của mình) thì:
- * IE11 cho dù không natively có tính năng 'promise' cũng đã chạy được code dùng 'promise'
- * Mặc dù chạy ok vậy, nhưng việc import cả đống polyfill vô sẽ làm cho main-bundle.js nặng, giải pháp là import cái cần xài thôi
- * main-bundle.js (giảm từ 849 KiB xuống 460KiB)
+ * - Mặc dù dùng babel plugin từng cái 1 cũng ok, mà có cách khác đưa trước 1 đống plugin sẵn rồi dùng thôi (preset)
+ * - Sau khi cài:
+ *      @babel/preset-env
+ *      @babel/runtime
+ *      @babel/plugin-transform-runtime
+ *      core-js@3
+ * + cấu hình lại .babelrc
+ * + require thêm ở file này
+ * thì cứ code thoải mái theo kiểu modern
  */
-const myFunc = async () => {
-  await console.log("Hi from the future!");
+const myFunc = async (args) => {
+  //   const { a, b } = args;
+
+  await console.log("Hi from the future!" /*, a, b*/);
   console.log("Done!");
 };
-myFunc();
+myFunc({ a: 111, b: 222 });
